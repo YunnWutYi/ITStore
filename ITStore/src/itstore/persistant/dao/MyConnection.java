75a592edcc3d3850ce5cf16d74ffc888e98ad733
Yunn <Yunn@192.168.99.174>
2022-08-27 21:28:00 +0530

    Add DTO and connection

---
+++ ITStore/src/itstore/persistant/dao/MyConnection.java
@@ -0,0 +1,21 @@
+package itstore.persistant.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+	static Connection con=null;
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/itstore","root","11111");
+			System.out.println("Connecting...");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Driver class not found");
+		} catch (SQLException e) {
+			System.out.println("Database not found");
+		}
+		return con;
+	}
+}
---
+++ ITStore/src/itstore/persistant/dto/ITStoreRequestDTO.java
@@ -0,0 +1,139 @@
+package itstore.persistant.dto;
+
+public class ITStoreRequestDTO {
+	private int id;
+	private String brand;
+	private String model;
+	private String display;
+	private String cpu;
+	private String ram;
+	private String storage;
+	private String battery;
+	private String weight;
+	private String mainCamera;
+	private String frontCamera;
+	private String sound;
+	private String color;
+	private int price;
+	private int qty;
+	private String img;
+	private String gpu;
+	private String os;
+	private String type;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getDisplay() {
+		return display;
+	}
+	public void setDisplay(String display) {
+		this.display = display;
+	}
+	public String getCpu() {
+		return cpu;
+	}
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}
+	public String getRam() {
+		return ram;
+	}
+	public void setRam(String ram) {
+		this.ram = ram;
+	}
+	public String getStorage() {
+		return storage;
+	}
+	public void setStorage(String storage) {
+		this.storage = storage;
+	}
+	public String getBattery() {
+		return battery;
+	}
+	public void setBattery(String battery) {
+		this.battery = battery;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public String getMainCamera() {
+		return mainCamera;
+	}
+	public void setMainCamera(String mainCamera) {
+		this.mainCamera = mainCamera;
+	}
+	public String getFrontCamera() {
+		return frontCamera;
+	}
+	public void setFrontCamera(String frontCamera) {
+		this.frontCamera = frontCamera;
+	}
+	public String getSound() {
+		return sound;
+	}
+	public void setSound(String sound) {
+		this.sound = sound;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getGpu() {
+		return gpu;
+	}
+	public void setGpu(String gpu) {
+		this.gpu = gpu;
+	}
+	public String getOs() {
+		return os;
+	}
+	public void setOs(String os) {
+		this.os = os;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+
+}
